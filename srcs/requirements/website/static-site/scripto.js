ce0e5e14e76183b3e0219f314e584db124d81c6f
Votre Nom <Vous@exemple.com>
2024-08-30 17:37:55 +0200

    first 'Pong'

--- srcs/requirements/website/static-site/scripto.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    const button = document.getElementById('clickable');
-    const image = document.getElementById('hiddenImage');
-
-    button.addEventListener('click', () => {
-        image.style.display = 'block';
-    });
-});
