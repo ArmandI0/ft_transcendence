2da2d4f424148c8381271938644906bedd183611
Armand <aranger@42>
2024-09-05 12:11:39 +0200

    fix : path of templates and rename django service to web service

--- srcs/requirements/web/volume/app/settings.py
+++ srcs/requirements/web/volume/app/settings.py
@@ -16,7 +16,7 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+print(BASE_DIR)
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'app.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR), 'app/templates'],
+        'DIRS': [os.path.join((BASE_DIR), 'app' , 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
