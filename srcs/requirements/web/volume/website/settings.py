bad5a5019bd255f7a9fc110a70e2a95ffb92a576
Armand <aranger@42>
2024-09-06 12:21:01 +0200

    fix : rename app -> website

--- srcs/requirements/web/volume/website/settings.py
+++ srcs/requirements/web/volume/website/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'game',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -51,7 +52,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'app.urls'
+ROOT_URLCONF = 'website.urls'
 
 TEMPLATES = [
     {
@@ -69,7 +70,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'app.wsgi.application'
+WSGI_APPLICATION = 'website.wsgi.application'
 
 
 # Database
