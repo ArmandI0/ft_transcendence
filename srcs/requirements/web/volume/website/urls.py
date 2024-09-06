e912804d39aaadd6807f20ba587fe8f87e7ef1a9
Armand <aranger@42>
2024-09-06 13:38:20 +0200

    fix : path of apps

--- srcs/requirements/web/volume/website/urls.py
+++ srcs/requirements/web/volume/website/urls.py
@@ -21,5 +21,5 @@ from .views import index
 urlpatterns = [
     path('', index, name="index"),
     path('admin/', admin.site.urls),
-    path('game/' , include('game.urls')),
+    path('game/' , include('apps.game.urls')),
 ]
