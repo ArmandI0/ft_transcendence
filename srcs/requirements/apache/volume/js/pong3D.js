b0803c38e2f3057eb29d070b73d91ce11ef845c5
Nico Bureau <nico.bureau@email.com>
2024-09-27 19:36:02 +0200

    3D update

--- srcs/requirements/apache/volume/js/pong3D.js
+++ srcs/requirements/apache/volume/js/pong3D.js
@@ -11,6 +11,7 @@ function hideSectionPong(sectionId)
 document.getElementById('button-start-pong-3D').addEventListener('click', function() 
 {
     hideSectionPong('button-start-pong-3D');
+	document.getElementById('grid-3d-render').style.display = 'grid';
     startGame3D();
 });
 
@@ -62,6 +63,7 @@ const pad1_z = -40;
 const pad2_z = 40;
 const table_geom = new XYZ(40, 1, 80);
 const ball_start_dir = new XYZ(0, 0, 0.5);
+let clock = 0;
 
 function makeObjectInstance(geomType, geom, color, pos_z, scene) 
 {
@@ -75,10 +77,19 @@ function makeObjectInstance(geomType, geom, color, pos_z, scene)
 			geometry = new THREE.SphereGeometry(geom.getX(), geom.getY(), geom.getZ());
 			break;
 	}
-    const material = new THREE.MeshPhongMaterial(
+    const material = new THREE.MeshPhysicalMaterial(
 	{
         color: color,
-        emissive: color
+        emissive: color,
+		roughness:0.0,
+		metalness:1.0,
+		ior:1.0,
+		reflectivity:1.0,
+		transparent:true,
+		opacity:0.75,
+		alphaTest:0.41,
+		clearcoat:0.5,
+		clearcoatRoughness:0,
     });
 
 	const obj = new THREE.Mesh(geometry, material);
@@ -90,30 +101,25 @@ function makeObjectInstance(geomType, geom, color, pos_z, scene)
 function makeTable(scene)
 {
 	const glassMaterial = new THREE.MeshPhysicalMaterial({
-		color: 0xffffff,          // Couleur de base du matériau
-		clearcoat: 1.0,           // Effet de couche de finition, simule l'éclat du verre
-		clearcoatRoughness: 0.1,  // La rugosité de la couche de finition, réduit pour un effet plus lisse
-		metalness: 0.1,           // Réduit la métallisation pour simuler un effet de verre non métallique
-		roughness: 0.1,           // Réduit la rugosité pour un effet de surface lisse
-		transmission: 0.5,        // Le niveau de transmission de la lumière, 1.0 pour un matériau complètement transparent
-		opacity: 0.5,             // Opacité totale du matériau, 1.0 pour totalement opaque
-		transparent: true         // Le matériau doit être transparent
+		color: 0x000000,          
+		clearcoat: 1.0,           
+		clearcoatRoughness: 0.1,  
+		metalness: 0.1,       
+		roughness: 0.1,         
+		transmission: 0.5,        
+		opacity: 0.9,             
+		transparent: true        
 	});
 	
 	const geometry = new THREE.BoxGeometry(table_geom.getX(), table_geom.getY(), table_geom.getZ());
 	const table = new THREE.Mesh(geometry, glassMaterial);
 	const edges = new THREE.EdgesGeometry(geometry); 
-	const lines = new THREE.LineSegments(edges, new THREE.LineBasicMaterial( { color: 0xffffff } ) ); 	
+	const lines = new THREE.LineSegments(edges, new THREE.LineBasicMaterial( { color: 0xff4afa } ) ); 	
 	scene.add(table);	
 	scene.add(lines);
 	table.position.y = -1;
-	lines.position.y = -1;	
+	lines.position.y = -1;
 	return table;
-}
-
-function prepareScene()
-{
-
 }
 
 function calculateYposition(ball_pos_z)
@@ -128,63 +134,126 @@ function calculateYposition(ball_pos_z)
 
 function checkCollisionPad(ball, pad1, pad2)
 {
-	const pad1_right = pad1.position.x - pad_geom.getX(); 
-	const pad1_left = pad1.position.x + pad_geom.getX(); 
-	const pad2_right = pad2.position.x - pad_geom.getX(); 
-	const pad2_left = pad2.position.x + pad_geom.getX();
+	const pad1_right = pad1.position.x - pad_geom.getX()/2; 
+	const pad1_left = pad1.position.x + pad_geom.getX()/2; 
+	const pad2_right = pad2.position.x - pad_geom.getX()/2; 
+	const pad2_left = pad2.position.x + pad_geom.getX()/2;
 	const ball_right = ball.position.x - ball_geom.getX();
 	const ball_left = ball.position.x + ball_geom.getX();
 
-	if (ball.position.z < 0)
+	if (ball.position.z <= 0)
 	{
 		if (ball_left < pad1_right || ball_right > pad1_left)
-			return -1;
+			return -1000;
 		else
-		{
-			console.log(ball.position.x - pad1.position.x);
 			return (ball.position.x - pad1.position.x);
-		}
 	}
 	else
 	{
 		if (ball_left < pad2_right || ball_right > pad2_left)
-			return -1;
+			return -1000;
 		else
 			return (ball.position.x - pad2.position.x);
 	}
 }
 
-function updateBallPosition(ball, ball_dir, pad1, pad2)
+function updateBallPosition(ball, ball_dir, pad1, pad2, gridCollision)
 {
+	gridCollision.visible = false;
 	ball.position.x += ball_dir.getX();
 	ball.position.z += ball_dir.getZ();
 	ball.position.y = calculateYposition(ball.position.z);
-
+	const offset = 6;
 	let col_z = 0;
 
 	// check if wall hit
-	if (Math.abs(ball.position.x) >= table_geom.getX() / 2)
+	if (Math.abs(ball.position.x) >= table_geom.getX() / 2 - ball_geom.getX())
+	{
 		ball_dir.setX(-(ball_dir.getX()));
+		gridCollision.position.y = ball.position.y;
+		gridCollision.position.z = ball.position.z;
+		if (ball.position.x > 0)
+			gridCollision.position.x = table_geom.getX() / 2;
+		else if (ball.position.x <= 0)
+			gridCollision.position.x = - table_geom.getX() / 2;
+		gridCollision.visible = true;
+	}
 
 	// check if pad hit or ball out
 	if (Math.abs(ball.position.z) >= table_geom.getZ() / 2 - pad_geom.getZ() / 1.5)
 	{
 		col_z = checkCollisionPad(ball, pad1, pad2);
-		ball_dir.setZ(-(ball_dir.getZ()));
-		if (ball_dir.getX() > 0)
-			ball_dir.setX(Math.abs(col_z));
-		else if (ball_dir.getX() === 0)
-			ball_dir.setX(col_z);
+		// reset if out
+		if (col_z === -1000)
+		{
+			ball.position.z = 0;
+			ball.position.x = 0;
+			ball.position.y = calculateYposition(ball.position.z);
+			ball_dir.setX(0);
+			clock = Date.now();
+			pad1.position.x = 0;
+			pad2.position.x = 0;
+		}
 		else
-			ball_dir.setX(-Math.abs(col_z));
-
+		{
+			ball_dir.setZ(-(ball_dir.getZ()));
+			if (ball_dir.getX() > 0)
+				ball_dir.setX(Math.abs(col_z)/offset);
+			else if (ball_dir.getX() === 0)
+				ball_dir.setX(col_z/offset);
+			else
+				ball_dir.setX(-Math.abs(col_z)/offset);
+		}
 	}
-
-	// reset if out
-	if (col_z === -1)
-		ball.position.z = 0;
-
 	return ball_dir;
+}
+
+function makeEdges(geometry, color)
+{
+	const edges = new THREE.EdgesGeometry(geometry); 
+	const lines = new THREE.LineSegments(edges, new THREE.LineBasicMaterial( { color: color } ) ); 
+	return lines;
+}
+
+function makeGridCollision(spacing)
+{
+    const points = [];
+    const lineMaterial = new THREE.LineBasicMaterial({ color: 0xff8e3b });
+	const grid = new THREE.Group();
+
+    for (let i = -2; i <= 2; i++) 
+	{
+        for (let j = -2; j <= 2; j++) 
+		{
+            const point = new THREE.Vector3(0, i * spacing, j * spacing);
+            points.push(point);
+        }
+    }
+
+    for (let i = 0; i < points.length; i++) 
+	{
+        const currentPoint = points[i];
+        const x = currentPoint.x;
+        const y = currentPoint.y;
+
+        if ((i + 1) % 5 !== 0) 
+		{
+            const nextPointHoriz = points[i + 1];
+            const geometry = new THREE.BufferGeometry().setFromPoints([currentPoint, nextPointHoriz]);
+            const line = new THREE.Line(geometry, lineMaterial);
+			grid.add(line);
+        }
+
+        if (i < 20) 
+		{
+            const nextPointVert = points[i + 5];
+            const geometry = new THREE.BufferGeometry().setFromPoints([currentPoint, nextPointVert]);
+            const line = new THREE.Line(geometry, lineMaterial);
+			grid.add(line);
+        }
+    }
+
+    return grid;
 }
 
 async function  startGame3D()
@@ -195,12 +264,16 @@ async function  startGame3D()
 	const width_3d = container1.clientWidth;
 	const height_3d = container1.clientHeight;
 
-	const scene1 = new THREE.Scene();
+	const scene = new THREE.Scene();
 	const camera1 = new THREE.PerspectiveCamera(75, width_3d / height_3d, 0.1, 1000);
-	const renderer1 = new THREE.WebGLRenderer();
-
+	const renderer1 = new THREE.WebGLRenderer({
+		alpha:true,
+	});
+	
 	const camera2 = new THREE.PerspectiveCamera(75, width_3d / height_3d, 0.1, 1000);
-	const renderer2 = new THREE.WebGLRenderer();
+	const renderer2 = new THREE.WebGLRenderer({
+		alpha:true,
+	});
 
 	renderer1.setSize(width_3d,height_3d); 
 	container1.appendChild(renderer1.domElement);
@@ -211,54 +284,79 @@ async function  startGame3D()
 	let ball_dir = ball_start_dir;
 
 	// Adding objects
-	const pad1 = makeObjectInstance("box", pad_geom,0xff0000,pad1_z, scene1);
-	const pad2 = makeObjectInstance("box", pad_geom,0x0000ff,pad2_z, scene1);
-	const ball = makeObjectInstance("sphere", ball_geom,0xffffff,0, scene1);
-	const table = makeTable(scene1);
+	const pad1 = makeObjectInstance("box", pad_geom,0xff0000,pad1_z, scene);
+	const pad2 = makeObjectInstance("box", pad_geom,0x0000ff,pad2_z, scene);
+	const ball = makeObjectInstance("sphere", ball_geom,0xffffff,0, scene);
+	const table = makeTable(scene);
+	const geom = new THREE.BoxGeometry(pad_geom.getX(), pad_geom.getY(), pad_geom.getZ());
+	const linesEdgesPad1 = makeEdges(geom,0x4afff4);
+	const linesEdgesPad2 = makeEdges(geom,0xff7fa8);
+	linesEdgesPad1.position.z = pad1.position.z;
+	linesEdgesPad2.position.z = pad2.position.z;
+	scene.add(linesEdgesPad1);
+	scene.add(linesEdgesPad2);
+
+	const gridCollision = makeGridCollision(1);
+	gridCollision.visible = false;
+	scene.add(gridCollision);
 
 	// Adding light
-	const color = 0xFFFFFF;
+	const color = 0xFFFF00;
 	const intensity = 3;
-	const light = new THREE.DirectionalLight(color, intensity);
-	light.position.set(-10, 4, 0);
-	scene1.add(light);
+	// const light = new THREE.DirectionalLight(color, intensity);
+	// light.position.set(50, 50, 0);
+	// scene.add(light);
+
+	const spotLight = new THREE.SpotLight(color);
+	spotLight.position.set(0, 20, 10);
+	spotLight.angle = Math.PI / 6;
+	spotLight.penumbra = 0.1;
+	spotLight.target = table;
+	scene.add(spotLight);
 
 	camera1.position.z = 62;
 	camera1.position.y = 15;
 	camera2.position.z = -62;
 	camera2.position.y = 15;
 
-	/* cube1.position.z = 10;
-	cube2.position.z = -10; */
-
 	camera1.lookAt(0,0,0);
 	camera2.lookAt(0,0,0);
 
-	document.addEventListener('keydown', function(event) 
-	{
-		switch(event.code) 
-		{
-			case 'ArrowLeft':
-				pad1.position.x += 1;
-				break;
-			case 'ArrowRight':
-				pad1.position.x -= 1;
-				break;
-			case 'KeyA':
-				pad2.position.x -= 1;
-				break;
-			case 'KeyD':
-				pad2.position.x += 1;
-				break;			
-		}
+	let keysPressed = {};
+
+	document.addEventListener('keydown', function(event) {
+		keysPressed[event.code] = true;
+	});
+	
+	document.addEventListener('keyup', function(event) {
+		keysPressed[event.code] = false;
 	});
 
 	function animate() 
 	{
 		requestAnimationFrame(animate);
-		ball_dir = updateBallPosition(ball, ball_dir, pad1, pad2);
-		renderer1.render(scene1, camera1);
-		renderer2.render(scene1, camera2);
+		if(!clock)
+			ball_dir = updateBallPosition(ball, ball_dir, pad1, pad2, gridCollision);
+		else if(Date.now() - clock > 1500)
+			clock = 0;
+		linesEdgesPad1.position.x = pad1.position.x;
+		linesEdgesPad2.position.x = pad2.position.x;
+
+		if (keysPressed['ArrowLeft']) {
+			pad1.position.x += 0.75; 
+		}
+		if (keysPressed['ArrowRight']) {
+			pad1.position.x -= 0.75;
+		}
+		if (keysPressed['KeyA']) {
+			pad2.position.x -= 0.75;
+		}
+		if (keysPressed['KeyD']) {
+			pad2.position.x += 0.75;
+		}
+
+		renderer1.render(scene, camera1);
+		renderer2.render(scene, camera2);
 	}
 	animate();
 }
