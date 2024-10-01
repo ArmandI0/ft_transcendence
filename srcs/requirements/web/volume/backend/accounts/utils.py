96962e2a45e856ada7193b26bf21a25cdd13d48c
Kernicterus <kernicterus@kernicterus.com>
2024-10-01 17:51:16 +0200

    upd vault request

--- srcs/requirements/web/volume/backend/accounts/utils.py
+++ srcs/requirements/web/volume/backend/accounts/utils.py
@@ -35,6 +35,6 @@ def get_postgres_cred_dbuser_wo():
 		secret_data = response.json()
 		return secret_data, 200
 	else:
-		print("Vault request error:", response.status_code, response.text)
+		print("MSGError:", response.status_code, response.text)  # Afficher le message d'erreur
 		secret_data = None
 		return secret_data, response.status_code
