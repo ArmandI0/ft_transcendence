4c528075a006821fa084592cd98e680bc6ece535
Kernicterus <kernicterus@kernicterus.com>
2024-10-02 17:50:18 +0200

    maj char

--- srcs/requirements/apache/volume/js/charts.js
+++ srcs/requirements/apache/volume/js/charts.js
@@ -4,6 +4,8 @@ const data = [
     { y: 3, x:1 },
     { y: 4, x:1 },
     { y: 5, x:1 },
+    { y: 10, x:1 },
+    { y: 2, x:1 }
 ];
 
 const div_chart1 = document.getElementById('bars-score-by-game');
--- srcs/requirements/apache/volume/js/classes/ChartBar.js
+++ srcs/requirements/apache/volume/js/classes/ChartBar.js
@@ -5,22 +5,22 @@ export default class ChartBar
 		this.data_yx = data_yx;
 		this.html_container = html_container;
 		this.y_max = data_yx.reduce((max, obj) => obj.y > max ? obj.y : max, data_yx[0].y);
+		this.nb_obj = data_yx.length;
 	}
 
 	generateBars() 
 	{
-		console.log(this.y_max);
 		this.data_yx.forEach(obj => 
 		{
 			console.log(obj.y);
 			var newBar = document.createElement("div");
-			const prct = 100 * obj.y / this.y_max;
-			newBar.style.backgroundColor = ' blue' ;
-			newBar.style.height = `${prct}%`;
-			newBar.style.width = '10px';
-			newBar.style.marginRight = '10px';
+			const prct_height = 100 * obj.y / this.y_max;
+			const prct_width = 100 / this.nb_obj;
+			newBar.style.height = `${prct_height}%`;
+			newBar.style.width = `${prct_width - 5}%`;
+			newBar.style.marginLeft = '5%';
 			newBar.style.display = 'inline-block';
-			newBar.classList.add('bar');
+			newBar.classList.add('bar_chart');
 			this.html_container.appendChild(newBar);  
 		});
 	}
