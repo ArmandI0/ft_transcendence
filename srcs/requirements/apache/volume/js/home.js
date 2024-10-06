f93f1d647eedbcf5171c4383aedda528e4e09064
Armand <aranger@42>
2024-10-06 17:58:13 +0200

    feat : authentification work in frontend

--- srcs/requirements/apache/volume/js/home.js
+++ srcs/requirements/apache/volume/js/home.js
@@ -15,7 +15,7 @@ function handleCode(code)
 
 function updateUrlWindow(newUrl) 
 {
-	window.history.replaceState({ path: newUrl }, '', newUrl);
+	window.history.replaceState('app', '', newUrl);
 }
 
 export function handleAPI42return(url)
