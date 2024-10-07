4bfbbb54dc5ab0e6068258c3c57da062db91570a
aranger <aanger3@gmail.com>
2024-10-07 17:47:03 +0200

    add: script to load database + set view for api

--- srcs/requirements/web/volume/backend/api/serializers.py
+++ srcs/requirements/web/volume/backend/api/serializers.py
@@ -1,3 +1,13 @@
-from django.contrib.auth.models import User
 from rest_framework import serializers
+from .models import PongGameResult
 
+class PongChartResultSerializer(serializers.ModelSerializer):
+    player1_won = serializers.SerializerMethodField()
+    class Meta:
+        model = PongGameResult
+        fields = ['game_id' , 'player1_won' , 'time']
+    def get_player1_won(self, obj):
+        try:
+            return int(obj.score_player1) > int(obj.score_player2)
+        except ValueError:
+            return False
