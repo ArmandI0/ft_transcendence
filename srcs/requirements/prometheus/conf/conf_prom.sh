273239fa5c7d2e8791f190994b4d6c7522749410
aranger <aanger3@gmail.com>
2024-10-10 12:51:26 +0200

    fix : acces to prometheus with https

--- srcs/requirements/prometheus/conf/conf_prom.sh
+++ srcs/requirements/prometheus/conf/conf_prom.sh
@@ -2,4 +2,4 @@ while [ "$(curl -s -o /dev/null -w "%{http_code}" http://vault:8200/v1/sys/healt
 do sleep 1; 
 done
 
-./prometheus --config.file=/prometheus/prometheus.yml
+./prometheus --config.file=/prometheus/prometheus.yml --web.external-url=/prometheus
